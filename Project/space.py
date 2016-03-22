c074d3b9b4b9ca2457bb4ddef3db618b3a9ebea0
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-03-21 20:30:22 -0700

    brainstorming getting programming flowing

---
+++ Project/space.py
@@ -0,0 +1,43 @@
+""" 
+Sergio Avila
+
+In this program I will implement a versin of the  game space invaders
+for Game Development class. In this game there will be a main ship where 
+it will be able to shoot bullets to other smaller space ships.
+
+This project will be done in python and 
+it will be ran through the terminal/command prompt.
+
+For this program to run you will have to have python installed
+in your computer. However, if you use ubuntu python comes 
+already installed.
+
+How to run it:
+First go to your folder where search.py is in.
+For example: cd /Game-Dev/Project$ 
+once you are there you simply type:
+python search.py
+"""
+
+""" 
+(this is just for my notes, and will be deleted later on)
+Brainstorm:
+
+Right off the bat I know that we are going to need a few classes
+Possible classes
+	-main ship or mother ship
+	-small ships/enemies
+	-maybe a few bigger enemies (possible)
+	-bullets might only be a function or maybe a whole class
+
+"""
+
+from pygame import *
+
+class MotherShip():
+	def bullets(): #not sure if this will work like this might need to make this a class.
+
+class enemies():
+
+class biggerEnemies():
+
